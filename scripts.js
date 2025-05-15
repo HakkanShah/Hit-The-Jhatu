37ad360bac62c5a91cf5a75aee2bdf5253b44182
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-05-15 19:28:56 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -102,8 +102,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let clickBuffer = null;
     let isMuted = false;
     let lastMuteClickTime = 0;
-    const muteClickDelay = 300; // 300ms delay between mute clicks
-
+    const muteClickDelay = 300; 
     const jhatuUpload = document.getElementById('jhatu-upload');
     const ganduUpload = document.getElementById('gandu-upload');
     const jhatuImage = document.getElementById('jhatu-image');
