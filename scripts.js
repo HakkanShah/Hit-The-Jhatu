4b96ebc7c3ddf060556cd9787e14f04631b3e46b
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-05-24 21:17:46 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -800,7 +800,7 @@ document.addEventListener("DOMContentLoaded", function () {
         statisticsModal.style.display = 'none';
     });
 
-    // Initialize statistics
+    
     loadStatistics();
 
     
