822f70aeffdf11a848ae10de133f6d0a649e363d
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-05-26 19:24:59 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -514,7 +514,7 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
 
-       
+      
         const randomTime = baseTime * (0.8 + Math.random() * 0.4);
         
         setTimeout(() => {
