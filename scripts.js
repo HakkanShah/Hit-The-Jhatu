7ee93e4e7af5d6224b6f2aa2d1381c04c7a3d52d
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-05-26 19:24:23 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
             highScoreDisplay.textContent = highScore;
             localStorage.setItem("highScore", highScore);
             
-            
+           
             highScoreCelebration.style.display = 'block';
             setTimeout(() => {
                 highScoreCelebration.style.display = 'none';
