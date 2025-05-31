e9bd436b06aa3f6a5c9fd035b8e2ac43dfb90caa
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-05-31 20:09:08 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -129,8 +129,7 @@ document.addEventListener("DOMContentLoaded", function () {
             };
                   reader.readAsDataURL(e.target.files[0]);
         }
-    });
-    
+    });  
     ganduUpload.addEventListener('change', function(e) {
         if (e.target.files && e.target.files[0]) {
             const reader = new FileReader();
