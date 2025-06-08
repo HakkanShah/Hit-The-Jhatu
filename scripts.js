516864e57bbca2ca86942cebfb0ee0ab0f54e296
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-06-08 19:18:16 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -135,7 +135,7 @@ document.addEventListener("DOMContentLoaded", function () {
             reader.onload = function(event) {
                 
                 ganduImage.src = event.target.result;
-               
+              
               
                 customGanduImage = event.target.result;
    
