e617671ba1ec04c33507b0a68dd350f27df688c6
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-06-09 19:20:43 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -243,7 +243,7 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log('Error sharing:', err);
         }
     }
-   
+  
     muteButton.addEventListener("click", function(event) {
         event.stopPropagation();
         playClickSound();
