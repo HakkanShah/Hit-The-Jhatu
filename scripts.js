e03ac385056952b9a56a9c11e6deab33f391c108
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-07-02 21:01:31 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -120,7 +120,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 jhatus.forEach(jhatu => {
                     jhatu.src = customJhatuImage;
                 });
-             
+            
   
                 playClickSound();
             };
