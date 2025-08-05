51c4e748a131e599089e822504325b58933f4357
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-08-05 18:35:23 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -195,7 +195,6 @@ document.addEventListener("DOMContentLoaded", function () {
         isMuted = !isMuted;
         muteButton.querySelector('.button-text').textContent = isMuted ? '🔇' : '🔊';
        
-
         hitSound.volume = isMuted ? 0 : 1;
         clickSound.volume = isMuted ? 0 : 1;
         gameOverSound.volume = isMuted ? 0 : 1;
